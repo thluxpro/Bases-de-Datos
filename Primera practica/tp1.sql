f9b079740a95b9474bc3035bdcc2041c0f90aa87
lucas <lucas_ezes099@hotmai.com>
2019-08-15 22:02:23 -0300

    actualizacion

--- Primera practica/tp1.sql
+++ Primera practica/tp1.sql
@@ -42,7 +42,7 @@ FOREIGN KEY (codigo) references departamento
 
 --insert into trabajapara (dni,codigo,horas) values (29332501,2,10)
 --insert into trabajapara (dni,codigo,horas) values (19302500,1,8)
-
+--insert into trabajapara (dni,codigo,horas) values (29332501,1,10)
 --UPDATE empleado set genero = 'M'
 --where dni = 13334401;
 --SELECT * from empleado;
@@ -57,5 +57,35 @@ FOREIGN KEY (codigo) references departamento
 --select avg(sueldo)
 	--from trabajapara;
 --Select	
---CAST ((avg(sueldo)) as float) from empleado;
---Prueba gitkraken
+--CAST ((avg(sueldo)) as float) from empleado; -- cambiar tipo de variable
+--select distinct dni from trabajapara -- quitar dni repetidos
+
+--select distinct e.nombre, e.apellido, t.dni, t.codigo
+--from trabajapara t, empleado e -- reconocer la tabla de otra manera
+--where t.dni = e.dni; -- juntar 2 tablas 
+--punto f
+--select avg(sueldo) 
+--from empleado e, departamento d, trabajapara t 
+--where t.dni = e.dni and d.codigo = t.codigo and d.nombre = 'computo';  
+
+--punto g
+--select sum(horas)
+--from trabajapara t, departamento d
+--where d.nombre ='produccion' and t.codigo = d.codigo; 
+
+--select e.nombre 
+--from empleado e, departamento d, trabajapara t
+--where d.nombre='deposito' and d.codigo = t.codigo and t.dni = e.dni and horas >= 6;
+
+--select * from empleado
+--where sueldo between 20000 and 32000;
+
+--select * from empleado
+--where  nombre like 'R%R';
+
+--select * from empleado
+--where sueldo> 10000
+--order by (nombre,apellido) desc;
+
+--insert into empleado (dni,nombre,apellido,genero,sueldo) values (13334401,'RETAMAR','JOA' ,'F',35000)
+
